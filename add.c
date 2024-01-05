fc97f24131b8c21342af29d13f63a081ca01e7a1
GillesR76 <gilles-richard76@hotmail.com>
2024-01-05 10:36:40 +0100

    going back to Kens version as the changes have no impact

--- add.c
+++ add.c
@@ -7,12 +7,15 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
+	stack_t *temp;
+
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "l%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	(*stack)->next->n += (*stack)->n;
+	temp = (*stack)->next;
+	temp->n += (*stack)->n;
 	pop(stack, line_number);
 }
